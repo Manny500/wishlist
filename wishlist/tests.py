ffc88b50df4a6e5fa85f68df4b6fe9720f4bd1fa
manny500 <tenoriom54@yahoo.com>
2020-05-21 23:57:52 -0700

    Adding all unnitest for book

--- wishlist/tests.py
+++ wishlist/tests.py
@@ -1,5 +1,6 @@
 from app import app
 import unittest
+import json
 
 class FlaskTestCase(unittest.TestCase):
 
@@ -97,77 +98,100 @@ class FlaskTestCase(unittest.TestCase):
         self.assertTrue(b'Books' in response.data)
 
 ############################## API TEST ########################################
+    #Ensure login behaives correctly
+    def test_check_login(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/check_login',
+            data=json.dumps(dict(email="Manny@hdsalkfjal",password_candidate="12345678")),
+            follow_redirects=True,content_type='application/json')
+        self.assertEqual(response.content_type, 'application/json')
+
     #Ensure that we can retrive all books
     def test_all_book_status(self):
         tester = app.test_client(self)
-        response = tester.get('/book')
+        response = tester.get('/book?id=2')
         self.assertEqual(response.content_type, 'application/json')
 
     #Ensure that we can retrive all books     
     def test_all_books(self):
         tester = app.test_client(self)
-        response = tester.get('/book', follow_redirects=True)
-        self.assertIn(b'"author": "Potter"', response.data)
+        response = tester.get('/book?id=2', follow_redirects=True)
+        self.assertIn(b'"author": "Unknown"', response.data)
 
     #Ensure that we can retrive one book
     def test_book_status(self):
         tester = app.test_client(self)
-        response = tester.get('/book')
+        response = tester.get('/book/2')
         self.assertEqual(response.content_type, 'application/json')
 
-    #Ensure that we can retrive one book     
+   #Ensure that we can retrive one book
     def test_book(self):
         tester = app.test_client(self)
-        response = tester.get('/book', follow_redirects=True)
-        self.assertIn(b'"author": "Potter"', response.data)
+        response = tester.get('/book/2')
+        self.assertIn(b'"title": "Scarlet Letter"', response.data)
 
     #Ensure that we can update a book
     def test_update_book_status(self):
         tester = app.test_client(self)
-        response = tester.get('/book')
+        tester.put(
+            '/book/14',
+            data=json.dumps(dict(isbn="7926410343", title="Dubmo 2", author="Steven spils", date="1998")),
+            follow_redirects=True, content_type='application/json')
+        response = tester.get('/book/14')
         self.assertEqual(response.content_type, 'application/json')
 
     #Ensure that we can update a book    
     def test_update_book(self):
         tester = app.test_client(self)
-        response = tester.get('/book', follow_redirects=True)
-        self.assertIn(b'"author": "Potter"', response.data)
+        tester.put(
+            '/book/14',
+            data=json.dumps(dict(isbn="7926410434", title="Dubmo 2", author="Steven spils", date="1998")),
+            follow_redirects=True, content_type='application/json')
+        response = tester.get('/book/14')
+        self.assertIn(b'"title": "Dubmo 2"', response.data)
 
-    #Ensure that we can delete a book
-    def test_delete_book_status(self):
-        tester = app.test_client(self)
-        response = tester.get('/book')
-        self.assertEqual(response.content_type, 'application/json')
+    # #Ensure that we can delete a book
+    # def test_delete_book_status(self):
+    #     tester = app.test_client(self)
+    #     response = tester.delete('/book/6')
+    #     self.assertEqual(response.content_type, 'application/json')
 
-    #Ensure that we can delete a book    
-    def test_delete_book(self):
-        tester = app.test_client(self)
-        response = tester.get('/book', follow_redirects=True)
-        self.assertIn(b'"author": "Potter"', response.data)
+    # #Ensure that we can delete a book    
+    # def test_delete_book(self):
+    #     tester = app.test_client(self)
+    #     response = tester.delete('/book/6', follow_redirects=True)
+    #     self.assertIn(b'"author": "Potter"', response.data)
 
     #Ensure that we can create a book
     def test_create_book_status(self):
         tester = app.test_client(self)
-        response = tester.get('/book')
+        tester.post('/book', 
+            data=json.dumps(dict(isbn="019374692", title="NEW BOOK", author="Steven spils", date="1998", userId="3")),
+            follow_redirects=True, content_type='application/json')
+        response = tester.get('/book/15', follow_redirects=True)
         self.assertEqual(response.content_type, 'application/json')
 
     #Ensure that we can create a book    
     def test_create_book(self):
         tester = app.test_client(self)
-        response = tester.get('/book', follow_redirects=True)
-        self.assertIn(b'"author": "Potter"', response.data)
+        tester.post('/book', 
+            data=json.dumps(dict(isbn="019374692", title="NEW BOOK", author="Steven spils", date="1998", userId="3")),
+            follow_redirects=True, content_type='application/json')
+        response = tester.get('/book/15', follow_redirects=True)
+        self.assertIn(b'"author": "Steven spils"', response.data)
 
-    #Ensure that we can create a user
-    def test_create_user_status(self):
-        tester = app.test_client(self)
-        response = tester.get('/book')
-        self.assertEqual(response.content_type, 'application/json')
+    # #Ensure that we can create a user
+    # def test_create_user_status(self):
+    #     tester = app.test_client(self)
+    #     response = tester.get('/book')
+    #     self.assertEqual(response.content_type, 'application/json')
 
-    #Ensure that we can create a user    
-    def test_create_user(self):
-        tester = app.test_client(self)
-        response = tester.get('/book', follow_redirects=True)
-        self.assertIn(b'"author": "Potter"', response.data)
+    # #Ensure that we can create a user    
+    # def test_create_user(self):
+    #     tester = app.test_client(self)
+    #     response = tester.get('/book', follow_redirects=True)
+    #     self.assertIn(b'"author": "Potter"', response.data)
 
 if __name__ == '__main__':
     unittest.main()
